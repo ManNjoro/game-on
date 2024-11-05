fc4e6c7fae8c14bb2b4b99df56e8d3276d918446
Man Njoro <elijohnmwoho@gmail.com>
2024-11-05 22:25:33 +0300

    "Added state to track selected index in ListGroup component and updated list item rendering to reflect selection"

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Nairobi", "Tokyo", "Paris", "London"];
-  items = [];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
