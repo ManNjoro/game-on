d54049a31bf8297a6957ba3c5a76fb0e3522be63
Man Njoro <elijohnmwoho@gmail.com>
2024-11-06 01:02:29 +0300

    "Refactor ListGroup component: move styles to App.css, remove ListGroup.css and ListGroup.tsx files, and update App.tsx to use new ListGroup component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import Button from "./components/Button";
 import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  const items = ["Nairobi", "New York", "Paris", "London", "Tokyo"]
   const [isClicked, setIsClicked] = useState(false)
   const handleClick = () => setIsClicked(true);
   return (
     <div>
+      <ListGroup heading="Cities" items={items} onSelectItem={()=> console.log("clicked")} />
       {
         isClicked && (
           <Alert onClose={()=> setIsClicked(false)}>
