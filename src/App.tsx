1767fb1964f484b3ad74d0007cfda15870a24bd9
Man Njoro <elijohnmwoho@gmail.com>
2024-11-05 22:30:27 +0300

    "Moved items array and added heading prop to ListGroup component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+  let items = ["New York", "Nairobi", "Tokyo", "Paris", "London"];
+  return <div><ListGroup items={items} heading="Cities" /></div>
 }
 
 export default App;
