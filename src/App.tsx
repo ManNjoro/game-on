4bd239bee187ad7d372e78d34cdbd6fbe2cb8d98
Man Njoro <elijohnmwoho@gmail.com>
2024-11-05 23:01:29 +0300

    "Replace ListGroup component with Alert component in App.tsx, and add onSelectItem prop to ListGroup component in ListGroup.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "Nairobi", "Tokyo", "Paris", "London"];
-  return <div><ListGroup items={items} heading="Cities" /></div>
+  return (
+    <div>
+      <Alert>Hello <span>World</span></Alert>
+    </div>
+  );
 }
 
 export default App;
