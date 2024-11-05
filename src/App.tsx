32f453a6f2d98e0506dee61c7700200f5cea8e44
Man Njoro <elijohnmwoho@gmail.com>
2024-11-06 00:48:56 +0300

    "Added state to App component to toggle Alert component on button click, updated Alert component to include close button, and commented out Bootstrap CSS import in main.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false)
+  const handleClick = () => setIsClicked(true);
   return (
     <div>
-      <Button color="secondary" onClick={() => console.log("clicked")}>My Button</Button>
+      {
+        isClicked && (
+          <Alert onClose={()=> setIsClicked(false)}>
+            <p>This is a custom alert component.</p>
+          </Alert>
+        )
+      }
+      <Button color="secondary" onClick={handleClick}>My Button</Button>
     </div>
   );
 }
