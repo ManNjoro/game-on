b0be6c28fb8aee8603a42e5a4bf800a972e33313
Man Njoro <elijohnmwoho@gmail.com>
2024-11-07 14:49:17 +0300

    "Removed state and functionality from App.tsx, replaced with Form component import; added Bootstrap CSS import to main.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,9 @@
-import { useState } from "react";
-import produce from "immer";
-import { AiFillPlusSquare } from "react-icons/ai";
-import ExapandableText from "./components/ExapandableText";
+import Form from "./components/Form";
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
 
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
-  });
-
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
-  });
-
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = (id: number) => {
-    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed:true}: bug))
-    // setBugs(produce(draft => {
-    //   const bug = draft.find(bug => bug.id === 1)
-    //   if(bug){
-    //     bug.fixed = true
-    //   }
-    // }))
-    setGame({ ...game, player: { ...game.player, name: "Eli" } });
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Extra Cheese"] });
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
   return (
     <div>
-      <ExapandableText maxChars={40}>
-        Hello
-      </ExapandableText>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">Name</label>
+        <input type="text" name="" id="" className="form-control" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
