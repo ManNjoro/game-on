66112c936d13154bd86699b86bad46a438eca375
Man Njoro <elijohnmwoho@gmail.com>
2024-11-07 14:56:33 +0300

    "Added age input field and submit button to Form component, and imported index.css in main.tsx"

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,8 +3,13 @@ const Form = () => {
     <form>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input type="text" name="" id="" className="form-control" />
+        <input type="text" name="" id="name" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">Age</label>
+        <input type="number" name="" id="age" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Submit</button>
     </form>
   );
 };
