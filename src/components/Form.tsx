98585c31fa2e5df9f2fcddfdafff3e63736c56a4
Man Njoro <elijohnmwoho@gmail.com>
2024-11-07 14:59:30 +0300

    "Added FormEvent import and handleSubmit function to Form component, updated form element to call handleSubmit on submit, and reformatted label elements to have their text on a new line."

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,22 @@
+import { FormEvent } from "react";
+
 const Form = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
         <input type="text" name="" id="name" className="form-control" />
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
         <input type="number" name="" id="age" className="form-control" />
       </div>
       <button className="btn btn-primary">Submit</button>
