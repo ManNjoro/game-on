0655fc15a02c0595bc4f89795421920614039315
Man Njoro <elijohnmwoho@gmail.com>
2024-11-07 13:49:58 +0300

    "Added new state variables for game and pizza, and updated handleClick function to modify them. Also added rendering of game player name and pizza toppings to the UI."

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,36 @@ function App() {
     {id: 1, title: 'Bug 1', fixed: false},
     {id: 2, title: 'Bug 2', fixed: false},
   ])
+
+  const [game, setGame] = useState({
+    id:1,
+    player: {
+      name: "John"
+    }
+  })
+
+  const [pizza, setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['Mushroom']
+  })
+
   const handleClick = () =>{
     // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed:true}: bug))
-    setBugs(produce(draft => {
-      const bug = draft.find(bug => bug.id === 1)
-      if(bug){
-        bug.fixed = true
-      }
-    }))
+    // setBugs(produce(draft => {
+    //   const bug = draft.find(bug => bug.id === 1)
+    //   if(bug){
+    //     bug.fixed = true
+    //   }
+    // }))
+    setGame({...game, player: {...game.player, name:"Eli"}})
+    setPizza({...pizza, toppings: [...pizza.toppings, 'Extra Cheese']})
   }
   return (
     <div>
+      <p>{game.player.name}</p>
+      <ul>
+        {pizza.toppings.map((top, index) =><li key={index}>{top}</li> )}
+      </ul>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
