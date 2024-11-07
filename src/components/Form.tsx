14d939c7969f38189ddc19c58db3d0162f47b936
Man Njoro <elijohnmwoho@gmail.com>
2024-11-07 18:55:08 +0300

    "Updated Form component to use useState and onChange events instead of refs"

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,54 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  //   const nameRef = useRef<HTMLInputElement>(null);
+  //   const ageRef = useRef<HTMLInputElement>(null)
+  //   const person = {name: '', age: 0}
+  //   const handleSubmit = (e: FormEvent) => {
+  //     e.preventDefault();
+  //     if (nameRef.current !== null) person.name = nameRef.current.value;
+  //     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+  //     console.log(person);
+
+  //   };
+  const [person, setPerson] = useState({
+    name: "",
+    age: 0,
+  });
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log("Form submitted");
+    console.log(person);
+    
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input type="text" name="" id="name" className="form-control" />
+        <input
+          //   ref={nameRef}
+          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          type="text"
+          name=""
+          id="name"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input type="number" name="" id="age" className="form-control" />
+        <input
+        //   ref={ageRef}
+        onChange={e=> setPerson({...person, age: parseInt(e.target.value)})}
+          type="number"
+          name=""
+          id="age"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
