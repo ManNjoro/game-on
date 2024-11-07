1b5af24fc8b051ba1e9a6dc6062f6fae38a80092
Man Njoro <elijohnmwoho@gmail.com>
2024-11-08 00:10:11 +0300

    "Added ExpenseFilter component and implemented category filtering for ExpenseList"

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import { useState } from "react";
 import ExpenseTracker from "./components/ExpenseTracker";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('')
   const [expenses, setExpenses] = useState([
-    {id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
-    {id: 2, description: 'bbb', amount: 10, category: 'Utilities'},
-    {id: 3, description: 'ccc', amount: 10, category: 'Utilities'},
-    {id: 4, description: 'ddd', amount: 10, category: 'Utilities'},
-  ])
-  
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory ? expenses.filter(e=> e.category === selectedCategory) : expenses
   return (
     <div>
-      <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id)) } />
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
