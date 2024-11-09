8931c7e0eb10aa9f6e890ba364ed8ef04124f303
Man Njoro <elijohnmwoho@gmail.com>
2024-11-09 13:18:05 +0300

    "Added category selection to App component and passed selected category to ProductList component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import ProductList from "./components/ProductList";
 
 function App() {
-  
+  const [category, setCategory] = useState("");
   return (
     <div>
-      <ProductList />
+      <select
+        name=""
+        id=""
+        className="form-select"
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
-const ProductList = () => {
+const ProductList = ({category}: {category: string}) => {
     const [products,setProducts] = useState<string[]>([])
     useEffect(()=>{
-        console.log("fetching products");
+        console.log("fetching products in", category);
         setProducts(['Clothing', 'Household'])
         
-    })
+    }, [category])
   return (
     <div>ProductList</div>
   )
