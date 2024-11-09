77d6e44aa1fc14d33618d73a47a56912e598da13
Man Njoro <elijohnmwoho@gmail.com>
2024-11-09 22:13:41 +0300

    "Added type annotations, error handling, and rendering of user data in App component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
+interface User {
+  id: number;
+  name:string;
+}
 function App() {
-  const [users, setUsers] =useState([])
+  const [users, setUsers] =useState<User[]>([])
+  const [error, setError] = useState('')
 
   useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/users')
-  })
+    const fetchUsers = async() => {
+      try{
+
+        const res = await axios.get<User[]>('https://jsonplaceholder.typicode.com/xusers')
+        setUsers(res.data)
+      } catch (err) {
+        setError((err as AxiosError).message)
+      }
+    }
+    fetchUsers()
+  },[])
 
   return (
     <div>
-      
+      {error && <p className='text-danger'>{error}</p>}
+     <ul>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul> 
     </div>
   );
 }
