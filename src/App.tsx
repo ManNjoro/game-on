9a445d23d6c6ddd789f00036e0aaad6d8115440b
Man Njoro <elijohnmwoho@gmail.com>
2024-11-09 21:56:31 +0300

    "Removed ProductList component and select element, added axios import and users state, and modified useEffect hook to make API call"

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
 import { useEffect, useState } from "react";
-import ProductList from "./components/ProductList";
+import axios from "axios";
 
-const connect = () => console.log('Connecting');
-const disconnect = () => console.log('Disconnecting');
 function App() {
+  const [users, setUsers] =useState([])
+
   useEffect(()=>{
-    connect()
-    return ()=> disconnect()
+    axios.get('https://jsonplaceholder.typicode.com/users')
   })
-  const [category, setCategory] = useState("");
+
   return (
     <div>
-      <select
-        name=""
-        id=""
-        className="form-select"
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
+      
     </div>
   );
 }
