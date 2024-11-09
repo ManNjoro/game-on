a27f9865ead4c89c659cd194f796557c1634eac7
Man Njoro <elijohnmwoho@gmail.com>
2024-11-09 22:39:50 +0300

    "Added loading state, error handling, and CRUD functionality to user list component"

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,64 @@ import axios, { AxiosError, CanceledError } from "axios";
 
 interface User {
   id: number;
-  name:string;
+  name: string;
 }
 function App() {
-  const [users, setUsers] =useState<User[]>([])
-  const [error, setError] = useState('')
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-  useEffect(()=>{
-    const controller = new AbortController()
-    const fetchUsers = async() => {
-      try{
-
-        const res = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users', {signal: controller.signal})
-        setUsers(res.data)
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users",
+          { signal: controller.signal }
+        );
+        setUsers(res.data);
       } catch (err) {
-        if (err instanceof CanceledError) return
-        setError((err as AxiosError).message)
+        if (err instanceof CanceledError) return;
+        setError((err as AxiosError).message);
+      } finally {
+        setLoading(false);
       }
-    }
-    fetchUsers()
-    return () => controller.abort()
-  },[])
+    };
+    fetchUsers();
+    return () => controller.abort();
+  }, []);
+  console.log(isLoading);
+
+  const deleteUser = (user:User) => {
+    const originalUsers = [...users]
+    setUsers(users.filter(u=>u.id !=user.id))
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
+    .catch(err => {
+      setError(err.message)
+      setUsers(originalUsers)
+    })
+  }
+  const addUser = () => {
+    const newUser = {id: 0, name: 'Mosh'}
+    setUsers([newUser,...users])
+    axios.post('https://jsonplaceholder.typicode.com/users/', newUser)
+    .then(res => setUsers([res.data, ...users]))
+  }
 
   return (
     <div>
-      {error && <p className='text-danger'>{error}</p>}
-     <ul>
-      {users.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul> 
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li key={user.id} className="list-group-item d-flex justify-content-between">
+            {user.name}
+            <button className="btn btn-outline-danger" onClick={()=>deleteUser(user)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
